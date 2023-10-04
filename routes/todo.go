8372ea8372a985cf8152699db6a9737d77c41f2c
SHANKALPA KOIRALA <shankalpa.koirala@wesionary.team>
2023-10-04 11:03:44 +0545

    :recycle: Refactored the todo routes

--- routes/todo.go
+++ routes/todo.go
@@ -6,16 +6,14 @@ import (
 	"todo/middleware"
 )
 
-func SetupRoutes(handler *infrastructure.Handler, todoController *controller.TodoController, userController *controller.UserController, middleware *middleware.AuthMiddleware) {
+func SetupTodoRoutes(handler *infrastructure.Handler, todoController *controller.TodoController, userController *controller.UserController, middleware *middleware.AuthMiddleware) {
 	// handler.Gin.GET("/all-todo", todoController.GetAllTodo)
 
-	v1 := handler.Gin.Group("/")
+	todo := handler.Gin.Group("/todo").Use(middleware.ValidateToken())
 	{
-		v1.POST("/login", userController.Login)
-	}
-
-	v2 := handler.Gin.Group("/todo").Use(middleware.ValidateToken())
-	{
-		v2.GET("", todoController.GetAllTodo)
+		todo.POST("", todoController.CreateTodo)
+		todo.GET("/alltodo", todoController.GetAllTodo)
+		todo.PATCH("/:id", todoController.UpdateTodo)
+		todo.GET("/:id", todoController.GetTodoById)
 	}
 }
